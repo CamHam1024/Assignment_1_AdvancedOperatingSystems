f0ca7196ee07cadfe97e26547d20b46a5b2961f4
TinyBirdKami <cameronhamilton1@hotmail.co.uk>
2026-03-02 00:37:36 +0000

    Update Task_1_Resource_Management.sh
    Added rest of the menu optiosn for task 1
    -log file will archive after reaching 50MB in size
    -all other admin tasks have been added including proces termination

--- Task_1_Resource_Management.sh
+++ Task_1_Resource_Management.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 #Setting up base directories and file names
-#SELECTED_DIR = "$HOME"
-BASE_DIR="$HOME/AssignmentDirectory"
+#collects the directory path for the script itself (the logs will always be in the same place the script is)
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+#the directory for the logs
+BASE_DIR="$SCRIPT_DIR/logsDir"
+#directory for the log archive
 ARCHIVE_DIR="$BASE_DIR/ArchiveLogs"
+#directory for the log file
 LOG_FILE="$BASE_DIR/System_Monitor_Log.txt"
-WARNING_LIMIT_MB=500
+#log file warning
+warning=500000000 #in bytes
 
 #optional stuff :)
 RED='\033[1;31m'
@@ -16,20 +21,37 @@ NC='\033[0m'
 #Seting up the Admin Log function
 log_event(){
 local msg="$1 "
+echo
 printf "%s %s\n" "$(date '+%Y-%m-%d %H:%M:%S' ) " "$msg" >> "$LOG_FILE"
+printf "${NC}"
+echo "Log file has been updated."
+
+logSize=$(stat -c %s "$LOG_FILE")
+if [[ $(stat -c %s "$LOG_FILE") -gt warning  ]]; then
+	echo "Log file exceeded size limit, Archiving..." 
+
+	mkdir -p "$ARCHIVE_DIR" #create archive folder when needed
+	cd "$BASE_DIR" #set the directory to Base directory
+	LN="$(basename "$LOG_FILE")" #preserve the file name of the log file
+	zip -m archivedlog.zip "$LN"  #moves the log file to a zipped folder which is then created in the base dir
+	
+	# renaming and moving the archive
+	ZN=ArchivedLog-"$(date '+%Y-%m-%d %H:%M:%S' )".zip 
+	mv archivedlog.zip "$ZN" 
+	mv "$ZN" "$ARCHIVE_DIR" 
+fi
+echo
 }
 
 #Setting up the Menu
 print_menu(){
+printf "${GRN}Base Directory: $BASE_DIR${NC}"
+echo 
 printf "${YLW}"
-echo
 echo "============"
 echo "CCCU Data Center Process and Resource Management System."
-printf "${RED}"
-echo "t: Test print varaibles. Useful!"
-printf "${YLW}"
-echo "1: Current CPU and Memory Usage."
-echo "2: Display Directory Disk Usage"
+echo "1: Current system CPU and Memory Usage."
+echo "2: Base Directory Disk Usage"
 echo "3: List Highest Memory consuming Processes."
 echo "4: Terminate a process."
 echo "5: Exit"
@@ -37,17 +59,8 @@ echo "============"
 printf "${NC}"
 }
 
-test_print(){
-echo
-printf "HOME Var: ${RED}$HOME${NC}"
-echo
-printf "BASE Var: ${RED}$BASE_DIR${NC}"
-echo
-printf "LOG FILE Var: ${RED}$LOG_FILE${NC}"
-}
-
 cpu_usage(){
-printf "${GRN} Current CPU and Memory Useage ${NC}"
+printf "${GRN}Current CPU and Memory Useage ${NC}"
 echo
 CPU_USAGE=$(top -bn1 | awk '/Cpu/ { print $2 }') #calculates the current CPU Usage
 MEMORY_USAGE=$(free -m | awk '/Mem/ { print $3 }') #calculates the current Memory useage
@@ -57,30 +70,75 @@ printf "${GRN}CPU USEAGE: ${NC}" #CPU Usage %
 echo "$CPU_USAGE%"
 printf "${GRN}MEM USEAGE: ${NC}" #Memory Usage MB
 echo "$MEMORY_USAGE MB"
+sleep 1s
 
 #Log the event in the LOG FILE
 log_event "System CPU and MEMORY checked CPU: $CPU_USAGE%, MEM: $MEMORY_USAGE MB"
-echo "Log File Updated."
 }
 
 dir_usage(){
-echo "Not Implimented Yet"
+printf "${GRN}Directory Disk Useage${NC}"
+echo
+du -ch "$SCRIPT_DIR"
+sleep 1s
+#Log The Event
+log_event "$SCRIPT_DIR disk useage checked"
 }
 
 list_highest(){
-printf "${GRN} Top 10 CPU Consuming processes ${NC}"
+printf "${GRN}Top 10 CPU Consuming processes ${NC}"
 echo
-ps -eo cmd,pid,user,%cpu,%mem --sort=-%cpu | head
+ps -eo cmd,pid,nice,user,%cpu,%mem --sort=-%cpu | head
+sleep 1s
+#Log the event
 log_event "Top 10 CPU Hungry Processes checked"
-echo "Log File Updated"
 }
 
 terminate_process(){
-echo "Not Implimented Yet"
+#Select Process by its PID
+read -r -p "Please Type Process ID (Pid): " pid #type desired process
+
+if test -d /proc/$pid; then #Checks if the process Exists
+	read  nice cpu mem cmd < <(ps ho nice,%cpu,%mem,cmd $pid) #split the reading of ps into multiple variables 
+	echo
+	printf "${GRN}Process: $cmd ni: $nice CPU: $cpu Mem: $mem ${NC}\n" #Displays process
+	echo
+	
+	if  [[ $nice -ge 0 ]]; then #if the Nice value is greater or Equal to 0 ask to terminate
+		read -r -p "Would you like to terminate this process y/n: " TC
+		
+		if [[ "$TC" == "Y" || "$TC" == "y" ]]; then #ask to terminate
+			#Kill the process
+			kill -9 $pid
+			printf "${GRN}Process $pid has been terminated... ${NC}"
+			
+			sleep 1s
+			#Log the event
+			log_event "Process (pid: $pid , nice: $nice) has been terminated"
+			return
+		else
+			#Exit out of the command
+			printf "${RED}Command Cancelled... ${NC}"
+			sleep 1s
+			return
+		fi
+	else
+		#If the process is critical (below nice 0) refuse to terminate
+		printf "${RED}Cannot terminate a process with a priority of $nice\nOnly processes of 0 or above can be terminated "
+		
+		sleep 1s
+		#Log the event
+                log_event "Process (pid: $pid , nice: $nice) failed to terminate (Critcal process)"
+		return
+	fi
+else
+	printf "${RED}This Process doesn't exist...${NC}"
+	sleep 1s
+fi
 }
 
 exit_bye(){
-read -r -p "Are you Sure? y / n: " conf #Asks if you are sure you want to quit
+read -r -p "Are you Sure? y/n: " conf #Asks if you are sure you want to quit
 if [[ "$conf" == "Y" || "$conf" == "y"  ]]; then
 	echo "Bye Bye!" #If you type "Y" or "y" print out Bye Bye and exit the program
 	exit	
@@ -95,9 +153,15 @@ mkdir -p "$BASE_DIR"
 #checks to make sure  a log file exists
 if [[ ! -f "$LOG_FILE"  ]]; then
 	touch "$LOG_FILE"
-	printf "${GRN}System Log Created. At $LOG_FILE ${NC}"
+	
+	#dd if=/dev/zero of="$LOG_FILE" bs=1M count=5 status=none
+
+	log_event "Log Created"
+
+	printf "${GRN}System Log created"
 else
-	printf "${GRN}Log File Already Exists. ${NC}"
+	log_event "Management Tool Started."
+	printf "${GRN}Log file already exists in directory"
 fi
 
 while true; do
@@ -110,7 +174,7 @@ case "$choice" in #compairs the user input 'choice' with patterns in the case
 	3) list_highest ;;
 	4) terminate_process ;;
 	5) exit_bye ;;
-	t) test_print ;;
+	x) exit ;;
 	*) echo "Invalid Choice..." ;;
 	esac
 	echo
